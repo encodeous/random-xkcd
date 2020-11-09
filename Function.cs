c626452f3dc097732ff296b7ba9e502432cc65a8
encodeous <encodeous7@gmail.com>
2020-11-08 19:47:30 -0500

    Upload

---
+++ Function.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using CodeHollow.FeedReader;
+using CodeHollow.FeedReader.Feeds;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace random_xkcd
+{
+    public static class Function
+    {
+
+        [FunctionName("xkcd")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            // Read RSS Feed to obtain the latest id
+            var val = await FeedReader.ReadAsync("https://xkcd.com/atom.xml");
+            // Clean url
+            string pId = val.Items.First().Id.Substring(16).Replace("/","");
+            Random rand = new Random();
+            // Randomly pick a new id
+            int rng = rand.Next(1, int.Parse(pId) + 1);
+            var webClient = new WebClient();
+            // Load the xkcd website, and parse the html
+            var parser = new HtmlParser().ParseDocument(webClient.DownloadString($"https://xkcd.com/{rng}"));
+            // Grab the comic id
+            var container = parser.GetElementById("comic");
+            // In the child elements, find the src of the img tag
+            var tParse = container.GetElementsByTagName("img")[0].GetAttribute("src");
+            // Read the image into a memory stream
+            var ms = new MemoryStream(webClient.DownloadData("https://" + tParse.Substring(2)));
+            // Disable caching, (most sites like Github cache markdown and other embedded images)
+            req.HttpContext.Response.Headers.Add("Cache-Control", "no-store");
+            // Return image
+            return new FileStreamResult(ms, "image/png");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# random-xkcd
-A project that brings you a random xkcd post every time!
+# Random xkcd
+
+> xkcd is a webcomic of romance, sarcasm, math, and language.
+
+This project grabs a completely random comic at the click of a button.
+
+![A random xkcd](https://ec-xkcd.azurewebsites.net/api/xkcd)
+
+## tl;dr
+
+For markdown, add `![alt text](https://ec-xkcd.azurewebsites.net/api/xkcd)` to your profile page for a random comic every time you reload!
+
+For html, add `<img src="https://ec-xkcd.azurewebsites.net/api/xkcd" alt="alt text">` to the page.
+
+*Please note: If this api does not work, it probably means I have ran out of azure credits!*
+
+## API
+
+Send a http request to [https://ec-xkcd.azurewebsites.net/api/xkcd](https://ec-xkcd.azurewebsites.net/api/xkcd "https://ec-xkcd.azurewebsites.net/api/xkcd") and it will respond with a random xkcd as a png.
+
+## How it works
+
+1. On every HTTP request, the program reads the RSS feed of xkcd to obtain the latest post.
+2. Then, a random post, r is selected in the range `[1, x]` where x is the latest post.
+3. A HTTP request to `https://xkcd.com/<r>` is made, and the HTML is parsed.
+4. The image is extracted and then loaded into memory.
+5. The API then returns the extracted image.
